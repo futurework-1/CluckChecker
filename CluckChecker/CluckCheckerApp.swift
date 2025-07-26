2c1aece79cd305fd05b9cf62322f57a765419962
Adam <tabievadam@gmail.com>
2025-07-26 18:45:28 +0200

    1

---
+++ CluckChecker/CluckCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CluckCheckerApp.swift
+//  CluckChecker
+//
+//  Created by Адам Табиев on 26.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct CluckCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CluckChecker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CluckChecker
+//
+//  Created by Адам Табиев on 26.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
